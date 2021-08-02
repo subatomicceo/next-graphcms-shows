dc2a9e138d2b7ebdd44cf0a360f2f75282c61a12
Greg Parks <subatomicceo@gmail.com>
2021-08-01 19:31:45 -0700

    Issue #1 - Adds List and Toggle components to Shows;

--- pages/shows.js
+++ pages/shows.js
@@ -1,19 +1,39 @@
+import { useState } from 'react'
 import Layout from '@c/Layout'
 import { Grid, Card } from '@c/Grid'
+import { List } from '@c/List'
 import { Title } from '@c/Title'
+import { Toggle } from '@c/Toggle'
+import { TYPES } from '@c/Toggle/Toggle.consts'
 import { getAllShows } from '@l/graphcms'
 
+// constants
+const {
+  grid,
+  list
+} = TYPES
+
+
 export default function Shows({ shows }) {
+  const [toggle, setToggle] = useState(grid)
+
   return (
     <Layout title="next-graphcms-shows / Shows">
       <Title>Shows</Title>
-      <Grid>
-        {shows.map(show => (
-          <Card href={`/show/${show.slug}`} header={show.title} key={show.id}>
-            <p>{show.artists.map(({ fullName }) => fullName).join(', ')}</p>
-          </Card>
-        ))}
-      </Grid>
+
+      <Toggle toggle={toggle} setToggle={setToggle} />
+
+      {toggle === list ?
+        <List shows={shows} />
+        :
+        <Grid>
+          {shows.map(show => (
+            <Card href={`/show/${show.slug}`} header={show.title} key={show.id}>
+              <p>{show.artists.map(({ fullName }) => fullName).join(', ')}</p>
+            </Card>
+          ))}
+        </Grid>
+      }
     </Layout>
   )
 }
